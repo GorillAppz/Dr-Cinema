83f5e30490bb56a41113d106c642b95767a96b7f
Megg <arnimegg@gmail.com>
2019-12-09 18:17:44 +0000

    Main view

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-
+import { NavigationActions } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -9,13 +9,22 @@ import reducers from './src/reducers';
 import Footer from './src/components/Footer';
 
 const store = createStore(reducers, applyMiddleware(thunk));
+const navigation = React.createRef();
 
 export default function App() {
+	const handleNavigationChange = (screen) => {
+		navigation.current.dispatch(NavigationActions.navigate({
+			routeName: screen
+		}));
+	};
 	return (
 		<Provider store={store}>
 			<View style={{ flex: 1 }}>
-				<AppContainer />
-				<Footer />
+				<AppContainer
+					onNavigationStateChange={handleNavigationChange}
+					ref={navigation}
+				/>
+				<Footer navigationHandler={handleNavigationChange} />
 			</View>
 		</Provider>
 	);
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Button, Icon } from 'react-native-elements';
+
 import styles from './styles';
 
-const Footer = () => (
-	<View style={styles.container}>
-		<Button
-			icon={<Icon type="font-awesome" name="camera-retro" size={15} color="white" />}
-			title="Kvikmyndahús"
-			buttonStyle={styles.button}
-			titleStyle={styles.title}
-		/>
-		<Button
-			icon={<Icon type="font-awesome" name="film" size={15} color="white" />}
-			title="Væntanlegt"
-			buttonStyle={styles.button}
-			titleStyle={styles.title}
-		/>
-	</View>
-);
+const Footer = ({ navigationHandler }) => {
+	return (
+		<View style={styles.container}>
+			<Button
+				onPress={() => navigationHandler('Cinemas')}
+				icon={<Icon type="font-awesome" name="camera-retro" size={15} color="white" />}
+				title="Kvikmyndahús"
+				buttonStyle={styles.button}
+				titleStyle={styles.title}
+			/>
+			<Button
+				onPress={() => navigationHandler('Cinemas')}
+				icon={<Icon type="font-awesome" name="film" size={15} color="white" />}
+				title="Væntanlegt"
+				buttonStyle={styles.button}
+				titleStyle={styles.title}
+			/>
+		</View>
+	);
+};
 
 export default Footer;
