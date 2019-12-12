2a829b731610b9c032b42f24056a88f9a3f2b127
Daniel <danielevalberg@gmail.com>
2019-12-12 21:35:23 +0000

    added loading circle

--- src/components/TrailerListItem/index.jsx
+++ src/components/TrailerListItem/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import styles from './styles';
 import { trailerResultType } from '../../types';
 
+const loadingView = (
+	<View style={styles.loadingView}>
+		<ActivityIndicator size="large" />
+	</View>
+);
+
 const TrailerListItem = ({ trailer }) => (
 	<View style={styles.trailerContainer}>
 		<WebView
 			allowsFullscreenVideo
+			startInLoadingState
+			renderLoading={() => loadingView}
 			scrollEnabled={false}
 			source={{ uri: `https://www.youtube.com/embed/${trailer.key}` }}
+			style={styles.trailer}
 		/>
 	</View>
 );
--- src/components/TrailerListItem/styles.js
+++ src/components/TrailerListItem/styles.js
@@ -5,11 +5,21 @@ const window = Dimensions.get('window');
 export default StyleSheet.create({
 	trailerContainer: {
 		display: 'flex',
-		justifyContent: 'center',
+		alignContent: 'flex-start',
 		height: window.width * 0.80,
 		width: window.width * 0.925,
 		marginVertical: 10,
 		borderRadius: 20,
-		overflow: 'hidden'
+		overflow: 'hidden',
+		backgroundColor: 'black'
+	},
+	trailer: {
+		flex: 1,
+		backgroundColor: 'black',
+		justifyContent: 'flex-start'
+	},
+	loadingView: {
+		flex: 1,
+		backgroundColor: 'black'
 	}
 });
